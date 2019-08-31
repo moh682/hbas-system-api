4934a10764f9d67e1ee4eb687ebe0093e68accf5
cl0tCh <screenchok@gmail.com>
2019-08-31 12:28:37 +0200

    travis

---
+++ src/models/dbConnector.ts
@@ -0,0 +1,48 @@
+import mysql from 'mysql';
+
+let mysqlConfig: mysql.PoolConfig;
+if (process.env.ENV === "PRODUCTION") {
+   mysqlConfig = {
+      connectionLimit: 10,
+      host: 'localhost',
+      port: 3306,
+      user: 'prod',
+      password: '..prod2019!',
+      database: 'backend'
+   }
+}
+if (process.env.ENV === "TEST") {
+   mysqlConfig = {
+      connectionLimit: 10,
+      host: 'localhost',
+      port: 3306,
+      user: 'test',
+      password: '..test2019!',
+      database: 'backend'
+   }
+}
+if (process.env.ENV === "DEVELOPMENT") {
+   mysqlConfig = {
+      connectionLimit: 10,
+      host: 'localhost',
+      port: 3306,
+      user: 'dev',
+      password: '..dev2019!',
+      database: 'backend'
+   }
+}
+
+let pool: mysql.Pool;
+const getPool = () => {
+   if (pool) {
+      return pool
+   };
+   pool = mysql.createPool(
+      mysqlConfig
+   )
+   return pool;
+};
+
+export {
+   getPool
+}
